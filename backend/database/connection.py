82f9a223e2c31254d5483455146049f8ca3c91cd
saiennamuri <smartsunny314@gmail.com>
2026-09-25 10:57:44 +0530

    Fix Aiven MySQL connection

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -6,6 +6,7 @@ def get_db_connection():
 
     connection = mysql.connector.connect(
         host=Config.MYSQL_HOST,
+        port=Config.MYSQL_PORT,
         user=Config.MYSQL_USER,
         password=Config.MYSQL_PASSWORD,
         database=Config.MYSQL_DATABASE
